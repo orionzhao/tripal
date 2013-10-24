161b05f2538cb656155e7c0abcb690b15af38102
orion <orionzhao91@gmail.com>
2013-10-23 17:25:19 -0700

    all mvp views added

--- app/views.py
+++ app/views.py
@@ -5,6 +5,18 @@ from flask import render_template
 def index():
 	return render_template('index.html')
 
+@app.route('/city_forum/')
+def city_forum():
+	return render_template('city_forum.html')
+
+@app.route('/city_forum_discussion/')
+def city_forum_discussion():
+	return render_template('city_forum_discussion.html')
+
+@app.route('/city_list/')
+def city_list():
+	return render_template('city_list.html')
+
 @app.route('/example/')
 def example():
 	return render_template('example.html')
@@ -13,22 +25,42 @@ def example():
 def facebook():
 	return render_template('facebook.html')
 
+@app.route('/friend_list/')
+def friend_list():
+	return render_template('friend_list.html')
+
+@app.route('/friend_profile/')
+def friend_profile():
+	return render_template('friend_profile.html')
+
+@app.route('/friend_profile/')
+def friend_profile():
+	return render_template('friend_profile.html')
+
+@app.route('/log_in/')
+def log_in():
+	return render_template('log_in.html')
+
+@app.route('/nearby_traveler/')
+def nearby_traveler():
+	return render_template('nearby_traveler.html')
+
+@app.route('/non_friend_profile/')
+def non_friend_profile():
+	return render_template('non_friend_profile.html')
+
+@app.route('/private_message/')
+def private_message():
+	return render_template('private_message.html')
+
 @app.route('/profile/')
 def profile():
 	return render_template('profile.html')
 
-@app.route('/city_forum/')
-def city_forum():
-	return render_template('city_forum.html')
-
-@app.route('/city_list/')
-def city_list():
-	return render_template('city_list.html')
+@app.route('/profile_edit/')
+def profile_edit():
+	return render_template('profile_edit.html')
 
 @app.route('/settings/')
 def settings():
 	return render_template('settings.html')
-
-@app.route('/friend_list/')
-def friend_list():
-	return render_template('friend_list.html')
